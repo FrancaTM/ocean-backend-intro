a6e8a8b000f7a5443c830f3ce41d66396e308810
FrancaTM <francatm@outlook.com>
2021-02-04 02:21:44 -0300

    config mongodb connection

--- index.js
+++ index.js
@@ -1,64 +1,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
-app.use(bodyParser.json());
+(async () => {
+  const url = "mongodb://localhost:27017";
+  const dbName = "ocean_backend_intro_database";
 
-const port = 3000;
-const mensagens = [
-  {
-    id: 1,
-    texto: "Primeira mensagem",
-  },
-  {
-    id: 2,
-    texto: "Segunda mensagem",
-  },
-];
+  console.info("conectando ao banco de dados MongoDB...");
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  console.info("MongoDB conectado com sucesso");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+  const db = client.db(dbName);
 
-// Create
-app.post("/mensagens", (req, res) => {
-  const mensagem = req.body;
-  mensagem.id = mensagens.length + 1;
-  mensagens.push(mensagem);
-  res.send("mensagem criada com sucesso");
-});
+  const app = express();
 
-// Read all
-app.get("/mensagens", (req, res) => {
-  res.send(mensagens);
-});
+  app.use(bodyParser.json());
 
-// Read single
-app.get("/mensagens/:id", (req, res) => {
-  const id = +req.params.id;
-  const mensagem = mensagens.find((msg) => msg.id === id);
-  res.send(mensagem);
-});
+  const port = 3000;
 
-// Update
-app.put("/mensagens/:id", (req, res) => {
-  const id = +req.params.id;
-  const mensagem = req.body;
-  mensagem.id = id;
-  const index = mensagens.findIndex((msg) => msg.id === id);
-  mensagens[index] = mensagem;
+  const mensagens = db.collection("mensagens");
 
-  res.send("mensagem editada com sucesso");
-});
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
 
-// Delete
-app.delete("/mensagens/:id", (req, res) => {
-  const id = +req.params.id;
-  const index = mensagens.findIndex((msg) => msg.id === id);
-  delete mensagens[index];
-  res.send("mensagem removida com sucesso");
-});
+  // Create
+  app.post("/mensagens", (req, res) => {
+    const mensagem = req.body;
+    mensagem.id = mensagens.length + 1;
+    mensagens.push(mensagem);
+    res.send("mensagem criada com sucesso");
+  });
 
-app.listen(port, () => {
-  console.info("Servidor rodando em http://localhost:" + port);
-});
+  // Read all
+  app.get("/mensagens", (req, res) => {
+    // TODO: fix!
+    // res.send(await mensagens.find().toArray());
+  });
+
+  // Read single
+  app.get("/mensagens/:id", (req, res) => {
+    const id = +req.params.id;
+    const mensagem = mensagens.find((msg) => msg.id === id);
+    res.send(mensagem);
+  });
+
+  // Update
+  app.put("/mensagens/:id", (req, res) => {
+    const id = +req.params.id;
+    const mensagem = req.body;
+    mensagem.id = id;
+    const index = mensagens.findIndex((msg) => msg.id === id);
+    mensagens[index] = mensagem;
+
+    res.send("mensagem editada com sucesso");
+  });
+
+  // Delete
+  app.delete("/mensagens/:id", (req, res) => {
+    const id = +req.params.id;
+    const index = mensagens.findIndex((msg) => msg.id === id);
+    delete mensagens[index];
+    res.send("mensagem removida com sucesso");
+  });
+
+  app.listen(port, () => {
+    console.info("Servidor rodando em http://localhost:" + port);
+  });
+})();
